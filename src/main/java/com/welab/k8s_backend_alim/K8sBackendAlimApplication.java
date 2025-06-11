38d3a77a2c37be61925be3a44995f708ec5713e6
wkdnffla3 <wkdnffla3@gmail.com>
2025-06-11 16:56:40 +0900

    asd

--- src/main/java/com/welab/k8s_backend_alim/K8sBackendAlimApplication.java
+++ src/main/java/com/welab/k8s_backend_alim/K8sBackendAlimApplication.java
@@ -2,7 +2,9 @@ package com.welab.k8s_backend_alim;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+//@EnableFeignClients
 @SpringBootApplication
 public class K8sBackendAlimApplication {
 
